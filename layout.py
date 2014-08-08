58cae767437e50df3442bb5c8b70f9e4f0b7dda8
humberry <humberry@users.noreply.github.com>
2014-08-08 21:41:38 +0200

    Create layout.py

---
+++ layout.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+import ui
+
+def add_new_item(sender):
+  tf_new_item = view['new_item']
+  tv_shoppinglist = view['shoppinglist']
+  tv_shoppinglist.text += tf_new_item.text + '\n'
+  tf_new_item.text = ''
+
+view = ui.load_view('layout')
+view.present('fullscreen')
