54559fbde332e3865f280f0fcf71fece2a4b89bb
cclauss <cclauss@bluewin.ch>
2014-08-09 17:12:30 +0200

    Avoid adding blank lines to the shopping list
    Also, show that `present()` defaults to 'full-screen'.

--- shoppinglist.py
+++ shoppinglist.py
@@ -5,8 +5,10 @@ import ui
 def add_new_item(sender):
   tf_new_item = view['new_item']
   tv_shoppinglist = view['shoppinglist']
-  tv_shoppinglist.text += tf_new_item.text + '\n'
-  tf_new_item.text = ''
+  new_item = tf_new_item.text.strip()
+  if new_item:  # avoid adding blank lines
+      tv_shoppinglist.text += new_item + '\n'
+      tf_new_item.text = ''
 
 view = ui.load_view('shoppinglist')
-view.present('fullscreen')
+view.present()
