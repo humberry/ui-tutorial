e9503b4206a47dcc2f4a923bbb453cd5d823165c
humberry <humberry@users.noreply.github.com>
2014-08-09 16:10:12 +0200

    Create pop-over2.py

---
+++ pop-over2.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+import ui
+
+class PopOverView(ui.View):
+  view_main = None
+  view_po = None 
+
+  def __init__(self):
+    PopOverView.view_main = ui.load_view('pop-over2')
+    PopOverView.view_main.present('fullscreen')
+
+  def quit(self, sender):
+    PopOverView.view_po = ui.load_view('po2')
+    PopOverView.view_po.present('popover',popover_location=(400,400))
+
+  def ask_user(self, sender):	#action method for both buttons (yes and no)
+    if sender.name == 'yes':
+      PopOverView.view_po.close()
+      PopOverView.view_main.close()
+    else:
+      PopOverView.view_po.close()
+
+PopOverView()
