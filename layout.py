611a7e3eb8717cf6b6bcaa77e85122bc5ba30827
cclauss <cclauss@bluewin.ch>
2014-08-10 23:06:24 +0200

    Added no_blank_lines() function
    Before tapping Add would add a blank line.

--- layout.py
+++ layout.py
@@ -2,11 +2,15 @@
 
 import ui
 
+def no_blank_lines(text):
+    return '\n'.join([line.strip() for line in text.splitlines() if line])
+
 def add_new_item(sender):
-  tf_new_item = view['new_item']
-  tv_shoppinglist = view['shoppinglist']
-  tv_shoppinglist.text += tf_new_item.text + '\n'
-  tf_new_item.text = ''
+    tf_new_item = view['new_item']
+    tv_shoppinglist = view['shoppinglist']
+    tv_shoppinglist.text += '\n' + tf_new_item.text
+    tv_shoppinglist.text = no_blank_lines(tv_shoppinglist.text)
+    tf_new_item.text = ''
 
 view = ui.load_view('layout')
 view.present('fullscreen')
