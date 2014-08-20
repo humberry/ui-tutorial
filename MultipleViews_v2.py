315ebaba04f7d62536e6e163608411a758eed962
cclauss <cclauss@bluewin.ch>
2014-08-20 03:30:32 +0200

    Present() in __init__, move ui work to did_load()

--- MultipleViews_v2.py
+++ MultipleViews_v2.py
@@ -3,14 +3,14 @@
 import ui
 
 class MultipleViews(ui.View):
-	
     def __init__(self):
         self.labelcounter = 0
-        self.add(None, text='First Run')
+        self.present()
 
     def did_load(self):
         self['bt_remove_label'].action = self.remove
         self['bt_add_label'].action = self.add
+        self.add(None, text='First Run')
 
     def draw(self):
         print self.name
@@ -19,19 +19,18 @@ class MultipleViews(ui.View):
 
     def remove(self, sender):
         label = self['Label']
-        if label != None:
+        if label:
             self.remove_subview(label)
             self.labelcounter -= 1
 
     def add(self, sender, text='Labeltext'):
         self.labelcounter += 1
-        label = ui.Label()
-        label.name = 'Label'
+        label = ui.Label(name='Label')
         label.text = text
-        label.x = self.labelcounter * 20
-        label.y = self.labelcounter * 20
+        label.x = label.y = self.labelcounter * 20
         self.add_subview(label)
 
-v = MultipleViews()
 v = ui.load_view()	# Custom View Class = MultipleViews
-v.present('fullscreen')
+
+# Extra credit: modify remove() to delete the newest label first
+#                              instead of the oldest label first.
