be905680c887977d4b64f5738e29d57fdd5280a1
cclauss <cclauss@bluewin.ch>
2014-09-01 08:20:08 +0400

    Minor cleanup

--- ShowTableView.py
+++ ShowTableView.py
@@ -1,9 +1,8 @@
 # coding: utf-8
 
-import ui, os, console
+import console, os, ui
 
 class ShowTableView(ui.View):
-
     def __init__(self):
         self.view = ui.load_view('ShowTableView')
         self.view.present('fullscreen')
@@ -12,39 +11,38 @@ class ShowTableView(ui.View):
 
     def bt_dir_action(self, sender):
         dir_items = os.listdir(os.getcwd())
-        lst = ui.ListDataSource(dir_items)
         tv1 = self.view['tableview1']
-        tv1.data_source = lst
-        tv1.delegate = lst
-        tv1.editing = False
-        lst.delete_enabled = False 
+        tv1.data_source = tv1.delegate = ui.ListDataSource(dir_items)
+        tv1.data_source.delete_enabled = tv1.editing = False
         tv1.reload_data()
 
     def bt_empty_action(self, sender):
         lst = ui.ListDataSource([])
         tv1 = self.view['tableview1']
-        tv1.data_source = lst
-        tv1.delegate = lst
-        tv1.editing = False
-        lst.delete_enabled = False 
+        tv1.data_source = tv1.delegate = ui.ListDataSource([])
+        tv1.data_source.delete_enabled = tv1.editing = False
         tv1.reload_data()
 
     def bt_picture_action(self, sender):
-        lst = ui.ListDataSource([{'title':'none','accessory_type':'none'},{'title':'checkmark','accessory_type':'checkmark'},{'title':'detail_button','accessory_type':'detail_button'},{'title':'detail_disclosure_button','accessory_type':'detail_disclosure_button'},{'title':'disclosure_indicator','accessory_type':'disclosure_indicator'},{'title':'image24 and checkmark','image':'ionicons-images-24','accessory_type':'checkmark'},{'title':'image32','image':'ionicons-alert-32'},{'title':'image256','image':'ionicons-alert-circled-256'},{'title':'frog','image':'Frog_Face'},{'title':'OwnImage','image':'Image/OwnImage.png'}])
+        lst = ui.ListDataSource([{'title':'none','accessory_type':'none'},
+                                 {'title':'checkmark','accessory_type':'checkmark'},
+                                 {'title':'detail_button','accessory_type':'detail_button'},
+                                 {'title':'detail_disclosure_button','accessory_type':'detail_disclosure_button'},
+                                 {'title':'disclosure_indicator','accessory_type':'disclosure_indicator'},
+                                 {'title':'image24 and checkmark','image':'ionicons-images-24','accessory_type':'checkmark'},
+                                 {'title':'image32','image':'ionicons-alert-32'},
+                                 {'title':'image256','image':'ionicons-alert-circled-256'},
+                                 {'title':'frog','image':'Frog_Face'},
+                                 {'title':'OwnImage','image':'Image/OwnImage.png'}])
         tv1 = self.view['tableview1']
-        tv1.data_source = lst
-        tv1.delegate = lst
-        tv1.editing = False
-        lst.delete_enabled = False 
+        tv1.data_source = tv1.delegate = lst
+        tv1.data_source.delete_enabled = tv1.editing = False
         lst.action = self.tv1_action
         tv1.reload_data()
 
     @ui.in_background
     def tv1_action(self, sender):
-        s = ''
         info = sender.items[sender.selected_row]
-        for i in info:
-            s += i + ' = ' + info[i] + '\n'
-        console.alert('info', s)
+        console.alert('info', '\n'.join(['{} = {}'.format(i, info[i]) for i in info]))
 
 ShowTableView()
